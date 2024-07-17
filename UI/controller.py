0a05dcf65d25d0efd0a2ad31104eb5cb2dbd39b8
AlessiaLombardoPolito <s283236@studenti.polito.it>
2024-07-17 17:47:03 +0200

    flight : slz mia

--- UI/controller.py
+++ UI/controller.py
@@ -9,10 +9,50 @@ class Controller:
         self._model = model
 
     def handle_graph(self, e):
-        pass
+        self._view.txt_result.controls.clear()
+        self._model.buildGraph()
+        numNodi, numArchi = self._model.getDetailsGraph()
+        self._view.txt_result.controls.append(ft.Text(f"Il grafo ha {numNodi} nodi e {numArchi} archi"))
+        pesomin, pesomax = self._model.getPesoMax_min()
+        self._view.txt_result.controls.append(ft.Text(f"Il peso minore è {pesomin} e il peso maggiore è {pesomax}"))
+        self._view._page.update()
 
     def handle_countedges(self, e):
-        pass
+
+
+        valore = self._view.txt_name.value
+        try :
+            intd = float(valore)
+
+        except ValueError:
+            self._view.txt_result.controls.append(ft.Text(f"Il numero inserito non è un valore numerico"))
+            self._view._page.update()
+
+            return
+
+        if (self._model.controllaSoglia(intd)):
+            archiMinori, archiMaggiori = self._model.contaArchi(intd)
+            self._view.txt_result.controls.append(ft.Text(f"Il numero di archi con peso minore della soglia è  {archiMinori}"))
+            self._view.txt_result.controls.append(ft.Text(f"Il numero di archi con peso maggiore della soglia è  {archiMaggiori}"))
+            self._view._page.update()
+
+        else:
+            self._view.txt_result.controls.append(ft.Text(f"Il valore inserito {valore} non è nel range consentito"))
+            self._view._page.update()
 
     def handle_search(self, e):
-        pass
+        valore = self._view.txt_name.value
+        try:
+            intd = float(valore)
+
+        except ValueError:
+            self._view.txt_result.controls.append(ft.Text(f"Il numero inserito non è un valore numerico"))
+            self._view._page.update()
+
+            return
+        if (self._model.controllaSoglia(intd)):
+            percorso = self._model.cercaPercorso(intd)
+            for p in percorso:
+                self._view.txt_result.controls.append(ft.Text(f"{p}"))
+        self._view._page.update()
+
--- model/model.py
+++ model/model.py
@@ -1,3 +1,85 @@
+import networkx as nx
+
+from database.DAO import DAO
+
+
 class Model:
     def __init__(self):
+        self._diGraph = nx.DiGraph()
+        self._allChromosomes = DAO.getAllChromosomes()
+        self._edges = DAO.getAllEdges()
+        self._allGenes = DAO.getAllGenes()
+        self.idMap = {}
+        self._valMin = 0
+        self._valMax = 0
+        self._listaPesi = []
+
+
+    def loadGenes(self):
+        self._allGenes = DAO.getAllGenes()
+        self.idMap = {}
+        for g in self._allGenes:
+            self.idMap[g.GeneID] = g.Chromosome
+
+    def buildGraph(self):
+        self._diGraph.clear()
+        lista = []
+        for chromosome in self._allChromosomes:
+            lista.append(chromosome)
+        self._diGraph.add_nodes_from(lista)
+        self.loadGenes()
+
+
+        for edge in self._edges:
+            v1 = edge[0]
+            v2 = edge[1]
+            peso = edge[2]
+            nodoP = self.idMap[v1]
+            nodoA = self.idMap[v2]
+            if (nodoP, nodoA) in self._diGraph.edges:
+                self._diGraph[nodoP][nodoA]["weight"] += peso
+            else:
+                self._diGraph.add_edge(nodoP,nodoA, weight = peso)
+
+
+
+    def getDetailsGraph(self):
+        return self._diGraph.number_of_nodes(), self._diGraph.number_of_edges()
+
+    def getPesoMax_min(self):
+        self._listaPesi = []
+        listaArchi= self._allChromosomes
+        for i in listaArchi:
+            for j in listaArchi:
+                if (i,j) in self._diGraph.edges:
+                    self._listaPesi.append(self._diGraph[i][j]["weight"])
+
+        self._listaPesi.sort()
+        self._valMin = self._listaPesi[0]
+        self._valMax = self._listaPesi[-1]
+        return self._listaPesi[0], self._listaPesi[-1]
+
+
+    def controllaSoglia(self, d):
+        if d>= self._valMin and d<= self._valMax:
+            return True
+        else: return False
+
+
+    def contaArchi(self,d):
+        archiMaggiori = 0
+        archiMinori = 0
+        for peso in self._listaPesi:
+            if peso>=d:
+                archiMaggiori += 1
+            else:
+                archiMinori += 1
+        return archiMinori, archiMaggiori
+
+
+
+    def cercaPercorso(self, valore):
+        pass
+
+    def ricorsione(self, parziale):
         pass
