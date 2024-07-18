a750d88b74231173a7c7e8dbf551f95996e0c2ea
AlessiaLombardoPolito <s283236@studenti.polito.it>
2024-07-18 08:16:32 +0200

    flight : slz mia

--- model/appunti.py
+++ model/appunti.py
@@ -40,4 +40,70 @@ per controllare il dato inserito
             self._view.txt_result.controls.append(ft.Text(f"Il numero inserito non è un valore numerico"))
             self._view._page.update()
 
-            """
+
+componente connessa di un grafo non orientato:
+    def getConnessa(self, v0int):
+        v0 = self._idMap[v0int]
+
+        #slz 1 : successori di v0 in dfs
+        successors = nx.dfs_successors(self._grafo, v0) #restituisce dizionario dei successori in una dfs
+        allSuccessors = []
+        for v in successors.values():
+            allSuccessors.extend(v)
+
+
+        #slz 2 : predecessori di v0 in dfs
+        predecessors = nx.dfs_predecessors(self._grafo, v0)
+
+
+        #slz 3 : conto i nodi dell'albero di visita del dfs
+        albero = nx.dfs_tree(self._grafo, v0)
+
+        #slz4 : node_connected_components
+        connComp = nx.node_connected_component(self._grafo, v0)
+
+        #il 3 e il 4 contengono anche il nodo di partenza, mentre il 2 e l'1 non lo contengono
+
+
+
+
+
+eventi on action
+self._ddAnno = ft.Dropdown(label="Anno", width=200, alignment=ft.alignment.top_left,
+                                   on_change=self._controller.handleDDYearSelection)
+       # nella consegna c'è
+        # scritto intercettando l’evento onAction, quindi in questo menù scriviamo on_change che cambia risultato
+        # ogni volta che cambio il contenuto della tendina, ed essendo un evento nel
+        # controller poi mettiamo la e
+
+
+
+
+Alla pressione del bottone “Test Connessione”:
+• verificare se è possibile raggiungere l’aeroporto di arrivo a partire da quello di partenza;
+• stampare un possibile percorso (se esiste) tra i due aeroporti.
+
+    def esistePercorso(self,v0,v1):
+        connessa = nx.node_connected_component(self._grafo, v0)
+        if v1 in connessa:
+            return True
+        else:
+            return False
+
+
+
+Permettere all’utente di selezionare,
+dall’apposita tendina, un album a1 tra quelli
+presenti nel grafo. Alla pressione del bottone
+“Analisi Componente”, si stampino:
+• la dimensione della componente connessa a cui appartiene a1;
+• la durata complessiva (in minuti) di tutti gli album appartenenti alla componente connessa di a1.
+
+    def getConnessaDetails(self, v0):
+        conn = nx.node_connected_component(self._graph, v0)
+        durataTOT = 0
+        for album in conn:
+            durataTOT += toMinutes(album.totD)
+
+        return len(conn), durataTOT"""
+
